f6b907c218588a58618ea55cd8fecec3a7eca7ae
ppiphd <pushprap00@naver.com>
2025-04-22 15:59:34 +0900

    fix

--- src/components/login/loginPage.vue
+++ src/components/login/loginPage.vue
@@ -1,59 +1,60 @@
 <script>
 import HeaderPage from "../headerPage.vue";
 import FooterPage from "../footerPage.vue";
-import signupPage from "./signupPage.vue";
 import router from "../../router/index.js";
-import store, {POPUP_TYPE, STORE_TYPE} from "../../store/index.js";
-import {ref} from "vue";
+import store, { POPUP_TYPE, STORE_TYPE } from "../../store/index.js";
+import { ref } from "vue";
 import axios from "axios";
-
-// import {ref} from "vue";
 
 export default {
   name: "loginPage",
-  components: {FooterPage, HeaderPage, signupPage},
+  components: {
+    HeaderPage,
+    FooterPage,
+  },
   setup() {
-    const goToSample = () => {
-      router.push("/samplePage");
-    };
-    const goToSungmi = () => {
-      router.push("/newsPage");
-    }
-    const signupPage = () => {
-      router.push("/signupPage");
-    }
-    const isPopup = ref(store.state.popupType)
+    const email = ref("");
+    const pw = ref("");
 
+    const goToSample = () => router.push("/samplePage");
+    const goToSungmi = () => router.push("/newsPage");
+    const goToSignup = () => router.push("/signupPage");
     const goToPop = () => {
-      store.commit(STORE_TYPE.popupType, POPUP_TYPE.WRITE_BOARD)
-    }
+      store.commit(STORE_TYPE.popupType, POPUP_TYPE.WRITE_BOARD);
+    };
+
+    const isPopup = ref(store.state.popupType);
+
     const onLogin = () => {
-      const userEmail = document.getElementById("email");
-      const userPassword = document.getElementById("pw")
-      axios({
-        method: 'post',
-        url: '/camp-api.calf.kr',
-        data: {
-          userEmail: '',
-          userPassword: ''
-        }
-      }).then((res) => {
-         res);
-      }).catch(error => {
-         error);
-        throw new Error(error);
-      });
-    }
+      axios
+        .post("/api/login", {
+          userEmail: email.value,
+          userPassword: pw.value,
+        })
+        .then(res => {
+          console.log("로그인 성공:", res.data);
+          // 로그인 성공 시 동작 추가
+          // 예: 토큰 저장, 페이지 이동 등
+        })
+        .catch(error => {
+          console.error("로그인 실패:", error);
+          alert("로그인에 실패했습니다.");
+        });
+    };
 
     return {
-      goToSample, goToSungmi, signupPage, goToPop, isPopup, onLogin,
+      email,
+      pw,
+      goToSample,
+      goToSungmi,
+      goToSignup,
+      goToPop,
+      isPopup,
+      onLogin,
     };
-  }
-
+  },
 };
-
 </script>
-
 
 <template>
   <!--  <button @click="goToSample">샘플 페이지 가기</button>-->
@@ -69,11 +70,11 @@ export default {
         <h2>로그인 해볼까요?</h2>
         <label>
           <span>Email</span>
-          <input type="email" placeholder="이메일" id="email" required/>
+          <input type="email" placeholder="이메일" id="email" required />
         </label>
         <label>
           <span>Password</span>
-          <input type="password" placeholder="비밀번호" id="pw" required/>
+          <input type="password" placeholder="비밀번호" id="pw" required />
         </label>
 
         <div class="submit">
@@ -81,14 +82,11 @@ export default {
           <button type="button" @click="onLogin">로그인</button>
         </div>
         <p class="forgot-pass">아이디/비밀번호 찾기</p>
-
       </div>
       <div class="sns-login">SNS 계정으로 간편 로그인/회원가입</div>
       <div class="sns-img">
-        <a href=""><img src="/assets/image/face.webp" alt="페이스북"></a>
+        <a href=""><img src="/assets/image/face.webp" alt="페이스북" /></a>
       </div>
     </form>
   </div>
-
 </template>
-
